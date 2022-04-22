c48ab3ab83669f911a357da090faa2689e8c17f6
aitor-arronte <aitoraalvarez@gmail.com>
2022-04-22 13:37:36 -1000

    added txt transcriptions and summaries

--- get_summaries.py
+++ get_summaries.py
@@ -2,6 +2,7 @@ from models.summarization import summarize_text
 from utils import speech_to_text as sp
 import os
 import settings
+import pandas as pd
 
 
 def process_transcripts(directory, lang='ru-RU'):
@@ -40,3 +41,34 @@ def create_summaries(transcripts_dir):
 			with open('summaries/'+t.replace('_translation.txt', '_summary.txt'), 'w') as fi:
 				for s in summaries:
 					fi.write(s[0]['summary_text'] + "\n")
+
+
+def get_txt_translations(data_file, lang='ru-RU'):
+	df = pd.read_excel(data_file, engine='openpyxl')
+	title_trans=[]
+	description_trans=[]
+	for i in range(0, len(df)):
+		if not pd.isna(df['ZITEMDESCRIPTIONWITHOUTHTML'][i]):
+			title_trans.append(sp.translate_text(df['ZCLEANEDTITLE'][i], lang))
+			description_trans.append(sp.translate_text(df['ZITEMDESCRIPTIONWITHOUTHTML'][i], lang))
+		else:
+			title_trans.append('')
+			description_trans.append('')
+	df_out = pd.DataFrame({'title_translation': title_trans, 'description_translation': description_trans, 'web_url': df['ZWEBPAGEURL'],
+	                       'audio_url':df['ZENCLOSUREURL'],'podcast_id':df['ZUUID'],'series_id':df['ZPODCASTUUID']})
+	df_out.to_excel('Podcasts_Translate.xlsx', engine='openpyxl')
+	return None
+
+
+def get_txt_summaries(data_file):
+	summaries=[]
+	df = pd.read_excel(data_file, engine='openpyxl')
+	for i in range(0, len(df)):
+		if not pd.isna(df['description_translation'][i]):
+			suma = summarize_text(df['description_translation'][i])
+			summaries.append(suma[0]['summary_text'])
+		else:
+			summaries.append('')
+	df['summaries'] = summaries
+	df.to_excel('Podcasts_Translate.xlsx', engine='openpyxl')
+	return None
