51ee233bfd43f177d51e54c12510ce6c0ad7ba2a
aitor-arronte <aitoraalvarez@gmail.com>
2022-06-30 16:22:50 -1000

    change download

--- utils/extract.py
+++ utils/extract.py
@@ -1,13 +1,11 @@
-import os
 import pandas as pd
 import requests
 
 
 def download_resources(data_file, direct):
-	assert os.path.isfile(data_file)
 	df = pd.read_excel(data_file, engine='openpyxl')
 	for i in range(0, len(df)):
 		audio_file = requests.get(df['ZENCLOSUREURL'][i])
-		with open(direct+df['ZUUID'][i], 'wb') as aud:
+		with open(direct+df['ZUUID'][i]+'.mp3', 'wb+') as aud:
 			aud.write(audio_file.content)
 	print("Audio files downloaded")
